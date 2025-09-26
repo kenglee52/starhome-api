8cd772141b5c5bec41cfa1e1cba204789c84f5a5
kenglee52 <leekeng943@gmail.com>
2025-09-26 19:52:57 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { connectDB } from "./models/index";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
