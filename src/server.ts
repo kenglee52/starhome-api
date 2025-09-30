8a29cfee6e89714305903fa30cc015462c48903e
kenglee52 <leekeng943@gmail.com>
2025-09-30 13:40:33 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { connectDB } from "./models/index";
 
-const PORT = Number(process.env.PORT);
+const PORT = Number(process.env.PORT) || 5000;
 
 connectDB().then(() => {
     app.listen(PORT, '0.0.0.0', () => {
