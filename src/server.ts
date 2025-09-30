b0f91869b231e60f6db96d32a2b6caebb5028110
kenglee52 <leekeng943@gmail.com>
2025-09-30 09:32:40 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from "./app";
 import { connectDB } from "./models/index";
 
-const PORT = process.env.PORT || 10000;
+const PORT = Number(process.env.PORT) || 10000;
+
 connectDB().then(() => {
-    app.listen(PORT,'0.0.0.0', () => {
-        console.log(`Server is running on ${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server is running on port ${PORT}`);
     });
-})
+});
